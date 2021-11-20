7c44c18fd60db2697aa4d5aeaefe0f6697d6a190
epalkaran <kaurpalk20@gmail.com>
2021-11-20 08:15:53 +0530

    3

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,70 @@
-// Grabbing the input
+// // Grabbing the input
 
-document.querySelector("button").addEventListener("click", function(){
-    var input = document.querySelector("input").value;
-    console.log(input);
-    pushToDOM(input);
+document.querySelector(".js-go").addEventListener("click", function(){
+
+     var input = document.querySelector("input").value;
+     // // doing the stuff with Api
+     var url = "http://api.giphy.com/v1/gifs/search?q=\"" +input+ "\"&api_key=dc6zaTOxFJmzC";
+     var GiphyAJAXCall = new XMLHttpRequest();
+     GiphyAJAXCall.open( 'GET', url );
+     GiphyAJAXCall.send();
+     GiphyAJAXCall.addEventListener('load',function(e)
+     {  
+        var a = document.querySelector(".js-container");
+        a.innerHTML =  ( " ");
+
+        var data = e.target.response;
+        pushToDOM(data);
+ 
+     }); 
 
 });
 
-document.querySelector("input").addEventListener('keyup', function(e){
-   
+
+
+document.querySelector(".js-userinput").addEventListener('keyup', function(e)
+{
        var input = document.querySelector("input").value;
-       if(e.which === 13){
-       console.log(input);
-       pushToDOM(input);
+       // // doing the stuff with Api
+       if(e.which === 13)
+       {
+          var url = "http://api.giphy.com/v1/gifs/search?q=\"" +input+"\"&api_key=dc6zaTOxFJmzC";
+          var GiphyAJAXCall = new XMLHttpRequest();
+          GiphyAJAXCall.open( 'GET', url );
+          GiphyAJAXCall.send();
+          GiphyAJAXCall.addEventListener('load',function(e)
+          {
+            var a = document.querySelector(".js-container");
+	        a.innerHTML =  ( " "); 
+            
+             var data = e.target.response;
+             pushToDOM(data);
+      
+          }); 
        }
 });
 
 
-// doing the stuff with Api
 
-// Show the GIF
 
+// // Show the GIF
 function pushToDOM(input)
-{
-    var container = document.querySelector(".js-container");
-    container.innerHTML = input;
+{   
+    var response = JSON.parse(input);
 
+    var imageUrl = response.data;
+
+    imageUrl.forEach(function(image){
+
+        var src = (image.images.fixed_height_small.url);
+
+        var container = document.querySelector(".js-container");
+        container.innerHTML = container.innerHTML +"<img src=\"" + src + "\"  class=\"container-image\" >";
+   
+    });
+
+    
 };    
+
+
+
